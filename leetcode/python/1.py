3da7ae9d62fc0bbfddc93948ff783fa765e910b0
Jintao <44796685+JintGuo@users.noreply.github.com>
2020-06-10 23:39:19 +0800

    Update 1.py
    remove the stupid solution for leetcode 1.py

--- leetcode/python/1.py
+++ leetcode/python/1.py
@@ -1,14 +1,8 @@
-"""
-not the best solution
-"""
-
 class Solution(object):
     def isPalindrome(self, x):
         """
         :type x: int
         :rtype: bool
         """
-        if x < 0: 
-            return False
-        return int(str(x)[::-1])+x == 2*x
+        return str(x)[::-1] == str(x)
         
