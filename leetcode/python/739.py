bf4f43bda23afd4221d15c10cc49263bf6341a1d
bubble <ls7198@163.com>
2020-06-12 11:03:45 +0800

    Update 739.py

--- leetcode/python/739.py
+++ leetcode/python/739.py
@@ -7,10 +7,12 @@ class Solution(object):
         """
         stack = []
         ret = [0 for _ in range(len(T))]
+        # 当前元素 < 栈顶元素，当前元素入栈
+        # 当前元素 > 栈顶元素，栈顶元素出栈，while 循环 直到栈顶元素 < 当前元素，当前元素入栈 
         for i, x in enumerate(T):
             while stack and T[stack[-1]] < x:
                 index = stack.pop()
-                ret[index] = i - index
+                ret[index] = i - index        # 出栈时索引距离即天数差
             stack.append(i)
         return ret
         
@@ -20,8 +22,9 @@ class Solution(object):
         :type T: List[int]
         :rtype: List[int]
         """
+        # 类似桶排序 https://zhuanlan.zhihu.com/p/109499004
         ret = [0 for _ in range(len(T))]
-        index = [0 for _ in range(101)]
+        index = [0 for _ in range(101)]         # 跟据温度值的范围[30,100] 设定桶的范围
 
         for i in range(len(T)-1, -1, -1):
             for j in range(T[i]+1, 101):
